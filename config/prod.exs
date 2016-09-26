8987f6105dd0d02dd42baa5fe81402f6fc758b7e
josuemx <artu_ja@hotmail.com>
2016-09-26 11:56:40 -0500

    for production

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,11 @@ use Mix.Config
 # which you typically run after static files are built.
 config :park_api, ParkApi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  uurl: [scheme: "https", host: "desolate-ravine-88557.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -56,6 +59,13 @@ config :logger, level: :info
 #     config :park_api, ParkApi.Endpoint, server: true
 #
 
+config :park_api, ParkApi.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
+
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
